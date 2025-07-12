fd553a796f399ceb7a28fee8d15fc577a6056b17
Piere0405 <jampierlml@gmail.com>
2025-07-11 23:34:01 -0500

    roles admin

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from services.usuario_service import registrar_usuario, mostrar_usuarios, iniciar_sesion
 from services.transaccion_service import flujo_cambio_divisas, ver_transacciones
 
-
 def menu_usuario(usuario):
     while True:
         print(f"\n=== Menú Usuario: {usuario.nombre} ===")
@@ -22,26 +21,43 @@ def menu_usuario(usuario):
             print("Opción no válida.\n")
 
 
-def main():
+def menu_admin():
     while True:
-        print("=== Aplicación de Cambio de Divisas ===")
-        print("1. Registrar usuario")
-        print("2. Mostrar usuarios registrados")
-        print("3. Iniciar sesión")
-        print("4. Salir")
+        print("\n=== Menú Administrador ===")
+        print("1. Ver usuarios registrados")
+        print("2. Cerrar sesión")
 
         opcion = input("Elige una opción: ")
 
         if opcion == "1":
-            registrar_usuario()
-        elif opcion == "2":
             mostrar_usuarios()
-        elif opcion == "3":
+        elif opcion == "2":
+            print("Sesión de administrador cerrada.\n")
+            break
+        else:
+            print("Opción no válida.\n")
+
+
+def main():
+    while True:
+        print("=== Bienvenido a la Aplicación de Cambio de Divisas ===")
+        print("1. Iniciar sesión")
+        print("2. Registrarse")
+        print("3. Salir")
+
+        opcion = input("Elige una opción: ")
+
+        if opcion == "1":
             usuario_actual = iniciar_sesion()
             if usuario_actual:
-                menu_usuario(usuario_actual)
-        elif opcion == "4":
-            print("Aplicación terminada!")
+                if usuario_actual.nombre.lower() == "admin":
+                    menu_admin()
+                else:
+                    menu_usuario(usuario_actual)
+        elif opcion == "2":
+            registrar_usuario()
+        elif opcion == "3":
+            print("¡Aplicación terminada!")
             break
         else:
             print("Opción no válida.\n")
